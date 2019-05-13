841a6dcedba98fc86595f070dbd26aea9683fe42
AndriiSkrypnik <skrypnik.andrii@gmail.com>
2019-05-13 12:12:16 +0300

    migration_start

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@
 
 Route::get('/', function () {return view('welcome');});
 Route::get('lara2', function () {return view('welcome2');});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
