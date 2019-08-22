06f7c04bd0ad1d7ba099f03bbb3ba05c9483d2e0
AndriiSkrypnik <skrypnik.andrii@gmail.com>
2019-08-22 17:58:05 +0300

    add_random_photo

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -52,8 +52,8 @@
         <div class="flex-center position-ref full-height">
             <div class="code">
                 @yield('code')
-            </div>
 
+            </div>
             <div class="message" style="padding: 10px;">
                 @yield('message')
             </div>
