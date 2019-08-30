821780fbf39af944fc5c4a76c4ce095d9d98e2b4
Shipukov Stanislav <shipukov.stanislav@gmail.com>
2019-08-30 14:04:45 +0300

    geo ip fix

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,6 +1,6 @@
 <div id="map"></div>
 <script type="text/javascript" src="{{ asset('js/map.js') }}"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_MAPS_API_KEY')}}&callback=initMap" async defer></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_MAPS_API_KEY')}}&callback=initUserMap" async defer></script>
 
 
 
